27e76ccd6e2472c4eed91218ababf3b22ec88db1
Duaa Tariq <tduaa34@gmail.com>
2024-08-07 16:07:56 -0400

    updated resume

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -3,7 +3,7 @@ import { FaDownload } from "react-icons/fa";
 const DownloadResume = () => {
   return (
     // Replace the href with the a link to your resume and replace the download attribute with your name
-    <a href="https://drive.google.com/uc?export=download&id=11BBnPR316kLTOcrdvLJSb_daXPJlBpVK
+    <a href="https://drive.google.com/uc?export=download&id=19rZf6-JZ7S2CLdn3dNOyB88izfNec8JI
     " download="DuaaTariq_Resume.pdf" className="flex items-center">
       <p className="mr-2">Download Resume</p>
       <FaDownload />
